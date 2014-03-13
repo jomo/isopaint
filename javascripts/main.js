86dc9e8342602cce6a8c5f6d554bc6c7a4160662
JonApps <hans.wurstchen@live.de>
2014-03-13 17:56:13 +0100

    wow

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,15 @@
 var pressed = new Array(10);
 var keys = [38,38,40,40,37,39,37,39,66,65];
 
-function foobar() {
-  console.log('test');
-}
-
 $(document).keydown(function(e) {
   pressed.push(e.keyCode);
   pressed.shift();
   if ( pressed.toString() == keys.toString() ) {
-    foobar();
+    $shibe = $('#shibe')
+    if ($shibe.length > 0) {
+        $shibe.remove();
+    } else {
+        $('body').append('<img src="/images/doge.png" id="shibe">');
+    }
   }
 });
