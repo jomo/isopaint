8213f4d10c66e428a530130fb1f551ca5957850c
JonApps <hans.wurstchen@live.de>
2014-03-13 12:24:07 +0100

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+var pressed = [];
+var keys = [38,38,40,40,37,39,37,39,66,65];
+
+function foobar() {
+  console.log('test');
+}
+
+$(document).keydown(function(e) {
+  keys.push( e.keyCode );
+  if ( pressed == keys ) {
+    foobar();
+    pressed = [];
+  }
+});
