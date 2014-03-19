63e88ddce7d767059b5c25333a132aef0d918d6d
JonApps <hans.wurstchen@live.de>
2014-03-19 11:11:27 +0100

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,7 @@
 var pressed = new Array(10);
-var keys = [38,38,40,40,37,39,37,39,66,65];
+var keys    = [38,38,40,40,37,39,37,39,66,65];
+var song    = new Audio('http://www.televisiontunes.com/themesongs/Jeopardy%20-%20Thinking%20Music.mp3');
+song.load();
 
 $(document).keydown(function(e) {
   pressed.push(e.keyCode);
@@ -9,8 +11,10 @@ $(document).keydown(function(e) {
     var $shibe = $('#shibe');
     if ($shibe.length > 0) {
         $shibe.remove();
+        song.pause();
     } else {
         $body.append('<img src="/images/doge.png" id="shibe">');
+        song.play();
     }
     $body.toggleClass('rainbow');
   }
